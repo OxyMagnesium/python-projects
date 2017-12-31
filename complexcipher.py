b08b3b926e447780a6b752c63c01bb17c265e99c
OmG <im.omgupta@gmail.com>
2017-12-31 22:11:45 +0530

    Added comments to make code easier to understand.

--- complexcipher.py
+++ complexcipher.py
@@ -1,37 +1,41 @@
-
 dictionary = ['0', '!', 'a', 'b', '1', 'c', 'd', '2', 'e', 'f', ',', 'g', 'h', '3', 'i', 'j','.', 'k', 'l', '4', 'm', ' ', 'n', '5', 'o', 'p', ':', 'q', 'r', '6', 's', 't', '\'', 'u', 'v', '7', 'w', 'x', '8', 'y', 'z', '?', '9']
 
-def convert(text):
-  text = text.strip()
+#Function start
+
+def convert(text): #Main function.
+  text = text.strip() #Setting up requirements for the algorithm.
   output = ""
   mult = 0
 
-  if function == "encode":
+  if function == "encode": #Checking whether to encode or decode.
     mult = 1
   elif function == "decode":
     mult = -1
 
-  for letter in range(len(text)):
+#Algorithm start
+
+  for letter in range(len(text)): #Main algorithm.
     uppercase = "N/A"
-    input = text[letter]
+    input = text[letter] #Taking the first character.
 
-    input = input.lower()
+    input = input.lower() #Making it lowercase.
 
-    if input == "`":
+    if input == "`": #"`" Represents a space, since a space would be otherwise deleted by line 6. This turns it back into a space.
       input = " "
 
-    if input not in dictionary:
+    if input not in dictionary: #Checking if the user entered an unsupported character.
       return "Sorry, \"" + input + "\" is not supported."
 
-    key = (dictionary.index(input) + mult)
-    if key > 42:
+    key = (dictionary.index(input) + mult) #Converts the input character into its index in the dictionary, and then adds the multiplier to it toconvert it to something else.
+
+    if key > 42: #Ensuring that the new index is in the range of the dictionary.
       key = (key - 42) - 1
     elif key < 0:
       key = (key + 42) + 1
 
-    output += dictionary[key]
+    output += dictionary[key] #Converting the new index to a character and adding it to the output string.
 
-    if mult > 0:
+    if mult > 0: #Increasing the multiplier to increase the scrambling. The multiplier must not exceed +-42 because if it does, then it will cause an error when added to the string.
       mult += 1
     elif mult < 0:
       mult -= 1
@@ -41,15 +45,21 @@ def convert(text):
     elif mult < -42:
       mult = -1
 
-  if output[0] == " ":
+#Algorithm end
+
+  if output[0] == " ": #If the first or last characters are spaces, they are converted to "`" so they aren't lost.
     output = "`" + output[1:len(output)]
   if output[(len(output) - 1)] == " ":
     output = output[0:(len(output) - 1)] + "`"
+
   return output
+
+#Function end
+#Program start
 
 print("")
 
-print("Welcome to ComplexCipher v1.2.1 by Om Gupta!")
+print("Welcome to ComplexCipher v1.2.2 by Om Gupta!")
 
 quit_wish = "y"
 
