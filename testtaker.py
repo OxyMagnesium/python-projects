f9657192985a2b0bd9c9fe56767761d432961477
OmG <im.omgupta@gmail.com>
2018-05-27 19:01:24 +0530

    Added support for saving test to archive, currently only works with a single set directory.

--- testtaker.py
+++ testtaker.py
@@ -1,39 +1,53 @@
+print("")
+
 question = 1
 total = 0
 correct = 0
 
-#import sys
-#import os
+output = ""
 
-#sys.stdout = open("target.txt", "w")
+import os
+
+os.chdir(r"C:\Users\Om Gupta\Documents\Test archives")
 
 while True:
 	intake = input("Question " + str(question) + ": ")
 	if intake == "end":
 		break
-	if intake == "1":
+	elif intake == "1":
+		intake_p = "Correct"
 		correct += 1
+	else:
+		intake_p = "Incorrect"
+	output += ("Question " + str(question) + ": " + intake_p + "\n")
 	total += 1
 	question += 1
 
 print("")
+output += "\n"
 
 print("You got " + str(correct) + "/" + str(total) + " questions correct.")
+output += ("You got " + str(correct) + "/" + str(total) + " questions correct.\n")
 print("Your percentage was " + str(round((correct/total)*100, 2)) + "%.")
+output += ("Your percentage was " + str(round((correct/total)*100, 2)) + "%.\n")
 
-#sys.stdout.close()
+print("")
+output += "\n"
 
-#save_bool = input("Do you want to archive this test? ")
+archive = open("newtest.txt", "w")
+archive.write(output)
+archive.close()
 
-#if save_bool == "yes":
-#        file_name = input("Enter archival name: ")
-#        os.rename("target.txt", file_name + ".txt")
-#        print("Test was archived as " + file_name + ".txt.")
-#else:
-#        os.remove("target.txt")
-#        print("Test was not archived.")
+save_bool = input("Do you want to archive this test? (Y/N): ")
+
+if save_bool.lower() == "y":
+		file_name = input("Enter archival name: ")
+		os.rename("newtest.txt", file_name + ".txt")
+		print("Test was archived as " + file_name + ".txt under Documents - Test archives.")
+else:
+        os.remove("newtest.txt")
+        print("Test was not archived.")
 
 print("")
 
-print("Press enter to exit.")
-
+end_stop = input("Press enter to quit.")
