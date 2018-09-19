688f1b66ba26f4c86245069c4749cd9f1dd0fc1d
OmG <im.omgupta@gmail.com>
2018-09-19 12:33:52 +0530

    Increased offset range to 4 digit numbers, greatly decreasing chance of repeats.

--- complexcipher.py
+++ complexcipher.py
@@ -6,18 +6,20 @@ dictionary = ['0', '!', 'a', 'b', '1', 'c', 'd', '2', 'e', 'f', ',', 'g', 'h', '
 
 def convert(text): #Main function.
   text = text.strip() #Setting up requirements for the algorithm.
-  offset_included = False
+  offset_included = 0
 
   if function == "encode" or function == "e": #Checking whether to encode or decode. If encoding, a random offset is added to the front of the string and used to offset the multiplier, increasing scrambling.
     random.seed()
-    offset = random.randint(1,9)
+    offset = random.randint(1,9999)
+    offset_l = len(str(offset))
 
     mult = 1 * offset
-    output = "%s" % offset
+    output = "%s%s" % (str(offset_l),str(offset))
 
   elif function == "decode" or function == "d": #If decoding, the initial offset is retrieved from the front of the string and that character is skipped by the algorithm.
-    offset = int(text[0])
-    offset_included = True
+    offset_l = int(text[0])
+    offset = int(text[1:(offset_l + 1)])
+    offset_included = offset_l + 1
 
     mult = -1 * offset
     output = ""
@@ -31,13 +33,13 @@ def convert(text): #Main function.
       input = " "
 
     if input not in dictionary: #Checking if the user entered an unsupported character.
-      return "Sorry, \"" + input + "\" is not supported."
+      return "Sorry, \"%s\" is not supported." % (input)
 
     key = (dictionary.index(input) + mult) #Converts the input character into its index in the dictionary, and then adds the multiplier to it to convert it to something else.
     key %= 42 #Ensuring that the index is in the range of the dictionary.
 
     output += dictionary[key] #Converting the new index to a character and adding it to the output string.
-    print("%s -> %s (%s,%s)" % (input,dictionary[key],mult,key))
+    print("%s -> %s (%s)" % (input,dictionary[key],key))
 
     offset += 1 #Increasing offset, increasing and flipping multiplier for maximum scrambling.
     if mult > 0:
@@ -59,7 +61,7 @@ def convert(text): #Main function.
 
 print("")
 
-print("Welcome to ComplexCipher v1.3.3 by Om Gupta!")
+print("Welcome to ComplexCipher v1.4.1 by Om Gupta!")
 
 quit_wish = "y"
 
